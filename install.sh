cc62beb93e4d06aa9a30fd8d5c987b07bca9d33c
myu <myu@myujiku.com>
2023-04-10 17:34:14 +0200

    create installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+####################
+# Config installer #
+####################
+
+# Stow directories to `$HOME`
+for dir in $(ls home); do
+    stow -d home -t $HOME $dir
+done
+
+# Stow directories to `$HOME/.config`
+for dir in $(ls config); do
+    stow -d config -t $HOME/.config $dir
+done
+
+# Install cargo dependencies
+cargo install fd-find starship --locked
