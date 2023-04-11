00d41c0364d23f806b75099e786d32f5f0195235
myu <myu@myujiku.com>
2023-04-11 11:53:01 +0200

    move stow into separate file

--- install.sh
+++ install.sh
@@ -1,18 +1,6 @@
-####################
-# Config installer #
-####################
-
 mkdir $HOME/.config/zsh/include -p
 
-# Stow directories to `$HOME`
-for dir in $(ls home); do
-    stow -d home -t $HOME $dir
-done
-
-# Stow directories to `$HOME/.config`
-for dir in $(ls config); do
-    stow -d config -t $HOME/.config $dir
-done
+./stow.sh
 
 # Install cargo dependencies
 cargo install fd-find starship --locked
--- stow.sh
+++ stow.sh
@@ -1,9 +1,9 @@
 # Stow directories to `$HOME`
 for dir in $(ls home); do
-    stow -Dd home -t $HOME $dir
+    stow -d home -t $HOME $dir
 done
 
 # Stow directories to `$HOME/.config`
 for dir in $(ls config); do
-    stow -Dd config -t $HOME/.config $dir
+    stow -d config -t $HOME/.config $dir
 done
