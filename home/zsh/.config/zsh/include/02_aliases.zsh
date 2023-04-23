26e84c2bb7f9638662d47f030e5e19a0587f2914
myu <myu@myujiku.com>
2023-04-23 22:48:07 +0200

    make ls colourful again

--- home/zsh/.config/zsh/include/02_aliases.zsh
+++ home/zsh/.config/zsh/include/02_aliases.zsh
@@ -3,7 +3,7 @@
 ###########
 
 alias c='clear'
-alias l='ls -AlFh --group-directories-first'
+alias l='ls -AlFh --group-directories-first --color=auto'
 
 # List zsh config file names
 alias zel='ls -1 $ZSH_CONF_DIR/include'
